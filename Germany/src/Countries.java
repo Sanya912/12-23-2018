b82b8aaeab4975c5063ef7d66dd1ff17ec644887
Sanya <melikhov912@gmail.com>
2019-02-06 23:21:03 -0500

    9

--- Germany/src/Countries.java
+++ Germany/src/Countries.java
@@ -8,7 +8,7 @@ public class Countries {
 		int a = 1;
 		int b = 2;
 		int sum =0;
-		for(int i = 0; i<5; i++) {
+		for(int i = 0; i<9; i++) {
 			System.out.println(sum+=1);
 			System.out.print("g");
 		}

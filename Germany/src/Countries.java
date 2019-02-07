0396b19234adb8d460858ea7c0c1dd8db00ea324
Sanya <melikhov912@gmail.com>
2019-02-06 23:28:10 -0500

    leters

--- Germany/src/Countries.java
+++ Germany/src/Countries.java
@@ -3,14 +3,14 @@ public class Countries {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String ger = "Germany";
-		String uk = "Ukraine";
+		String ger = "Gerany";
+		String uk = "Ukrne";
 		int a = 1;
 		int b = 2;
 		int sum =0;
-		for(int i = 0; i<9; i++) {
+		for(int i = 0; i<3; i++) {
 			System.out.println(sum+=1);
-			System.out.print("g");
+			System.out.print("fddff");
 		}
 
 	}
